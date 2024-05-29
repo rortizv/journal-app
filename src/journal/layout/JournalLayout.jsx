7f57b1a9598ba4d8865f2af2941b7860cfbd962f
Rafael Ortiz <rortiz@inkremental.co>
2024-05-29 11:04:18 -0500

    Navbar

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,18 @@
+import { Box } from '@mui/material'
+import { NavBar } from '../components';
+
+
+const drawerWidth = 240;
+
+export const JournalLayout = ({children}) => {
+  return (
+    <Box sx={{display: 'flex'}}>
+        <NavBar drawerWidth={drawerWidth}/>
+        {/* sidebar */}
+
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+            {children}
+        </Box>
+    </Box>
+  )
+}
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,7 +1,14 @@
-
+import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <div>JournalPage</div>
+    <JournalLayout>
+      <Typography variant="h4">Journal</Typography>
+
+      {/* Nothing selected */}
+
+      {/* NoteView */}
+    </JournalLayout>
   )
 }
