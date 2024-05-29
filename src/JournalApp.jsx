48333203788d83b630a68bb7cc18dbb699392e07
Rafael Ortiz <rortiz@inkremental.co>
2024-05-28 21:09:47 -0500

    Routes system

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,9 +1,7 @@
-
+import { AppRouter } from './router/AppRouter';
 
 export const JournalApp = () => {
   return (
-    <>
-        <h1>Journal App</h1>
-    </>
+    <AppRouter />
   )
 }
---
+++ src/auth/routes/AuthRoutes.jsx
@@ -0,0 +1,12 @@
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { RegisterPage, LoginPage } from '../pages';
+
+export const AuthRoutes = () => {
+  return (
+    <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/*" element={<Navigate to="/auth/login" />} />
+    </Routes>
+  )
+}
